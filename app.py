654cd3d628619ba050d00bf70977803492cfe6b3
Ryan Lin <linryan888@gmail.com>
2026-03-02 20:30:20 -0800

    login signup base code

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask
+from supabase import create_client, Client
+import os
+
+SUPABASE_URL = "https://skaanrwiwfnhofzdchiz.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrYWFucndpd2ZuaG9memRjaGl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE1NjIxMDUsImV4cCI6MjA4NzEzODEwNX0.8lV_Mncr3JADxVTJPnuhgVJs1PYsJh1Dj6iDpGTx6JY"
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = Flask(__name__)
+
+def signup(email, password):
+    response = supabase.auth.sign_up({
+        "email": email,
+        "password": password
+    })
+    return response
+
+def login(email, password):
+    response = supabase.auth.sign_in_with_password({
+        "email": email,
+        "password": password
+    })
+    return response
 
 @app.route("/")
 def home():
